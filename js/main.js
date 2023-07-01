dd3ac97533b87f947f2af2cc4956f2c5e1839789
Gabriel <89654616+gocendra@users.noreply.github.com>
2023-07-01 19:41:24 -0300

    update color de fondos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm navbar-light bg-light">
-<div class="container">
+<div class="container nav1">
   <a class="navbar-brand" href="productos.html">Navbar</a>
   <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
       aria-expanded="false" aria-label="Toggle navigation">
